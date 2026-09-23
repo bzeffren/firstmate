1ebf578d5c692810472b72415438215f76fe33b6
Brad Zeffren <brad@saturatedmediaworks.com>
2026-09-23 18:27:11 -0500

    no-mistakes(review): Decode serialized local note in captain-answer path

--- bin/fm-captain-hold.sh
+++ bin/fm-captain-hold.sh
@@ -962,6 +962,7 @@ apply_pending_retained_artifact() {  # <task-id>
         || { report_retained_artifact_failure "$id" "$marker"; return 1; }
       ;;
     --note)
+      [ "${args[1]}" != "local%20main" ] || args[1]="local main"
       data_abs=$(fm_backlog_data_absolute "$DATA") \
         && fm_backlog_deliverable_record "$DATA" "$data_abs" "$id" "${args[1]}" \
         || { report_retained_artifact_failure "$id" "$marker"; return 1; }
